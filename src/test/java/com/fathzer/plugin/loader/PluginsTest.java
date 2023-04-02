8912dbc89c9ebe2c5c6f6f9da1cb350f726ddaee
Fathzer <jm-dev@astesana.net>
2023-04-02 19:06:32 +0200

    Fixes test

--- src/test/java/com/fathzer/plugin/loader/PluginsTest.java
+++ src/test/java/com/fathzer/plugin/loader/PluginsTest.java
@@ -15,22 +15,22 @@ class PluginsTest {
 	@Test
 	void test() throws IOException {
 		final Plugins<Supplier> plugins = new Plugins<>(ClassLoader.getSystemClassLoader());
+		assertEquals(ClassLoader.getSystemClassLoader(), plugins.getClassLoader());
 		assertTrue(plugins.isEmpty());
 		assertEquals(0, plugins.getExceptions().size());
 		assertEquals(0, plugins.getInstances().size());
+		plugins.add(new PluginInstantiationException("Just a test"));
+		assertFalse(plugins.isEmpty());
 		final Supplier instance = () -> "a thing of mine";
 		plugins.add(instance);
 		assertFalse(plugins.isEmpty());
-		assertEquals(0, plugins.getExceptions().size());
+		assertEquals(1, plugins.getExceptions().size());
 		assertEquals(1, plugins.getInstances().size());
-		plugins.add(new PluginInstantiationException("Just a test"));
-		assertFalse(plugins.isEmpty());
 		
 		
 		Plugins<Supplier> plugins2 = new JarPluginLoader().getPlugins(Constants.OK_FILE, Supplier.class);
+		assertFalse(plugins2.isEmpty());
 		assertThrows(IllegalArgumentException.class, () -> plugins2.add(instance));
-		plugins2.add(new PluginInstantiationException("xxx"));
-		assertFalse(plugins.isEmpty());
 	}
 
 }
